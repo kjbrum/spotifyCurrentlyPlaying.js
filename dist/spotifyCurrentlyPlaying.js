2d4de219bd46bdfbbf4e2d3604bb3a17b0a391d9
Kyle Brumm <kjbrum@msn.com>
2016-02-17 17:37:44 -0600

    Fix container variable

--- dist/spotifyCurrentlyPlaying.js
+++ dist/spotifyCurrentlyPlaying.js
@@ -31,12 +31,14 @@
             this.queryLastfm(function() {
                 // Search Spotify for the track
                 self.searchSpotify(function() {
+                    // Check the type of selector that was supplied
+                    var container = self.selector;
+                    if(typeof(self.selector) === 'string') {
+                        container = document.querySelector(self.selector);
+                    }
+
                     // Display the iframe if we found a track URI
                     if(self.spotifyURI != '') {
-                        // Check the type of selector that was supplied
-                        if(typeof(self.selector) === 'string') {
-                            container = document.querySelector(self.selector);
-                        }
 
                         // Build the iframe element
                         var iframe = document.createElement('iframe');
